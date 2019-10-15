f5087648c1744f3ad8cd984b369bc8417921114a
YoloChocoPie <chocopieyolo@gmail.com>
2019-10-15 15:27:02 +0700

    ex06

---
+++ Ex06/Pro1/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro1
+{
+    public class Account
+    {
+
+        public Account(int amount)
+        {
+
+        }
+
+        public void Deposit(int amount)
+        {
+            
+
+        }
+
+        public void Withdraw(int amount)
+        {
+
+        }
+
+        public int Balance()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Ex06/Pro1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deposit test =
+        }
+    }
+}
